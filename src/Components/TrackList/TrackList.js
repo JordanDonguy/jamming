94d474fdf65ee8423546e0959b1a790d1a35fa90
Jordan <jordan.donguy@gmail.com>
2025-01-15 22:09:24 +0100

    In TrackList component, display userSearchResults tracks properties

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,10 +1,13 @@
 import React from "react";
 import styles from "./TrackList.module.css";
+import Track from "../Track/Track";
 
-function Tracklist () {
+function Tracklist (props) {
     return (
         <div className={styles.TrackList}>
-        {/* <!-- You will add a map method that renders a set of Track components  --> */}
+          {props.userSearchResults.map((track) => 
+            <Track track={track} key={track.id} />
+          )}
       </div>
     );
 }
