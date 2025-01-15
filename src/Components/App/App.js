03a9dbdae6a86adb861fe887d463b78a7ae10e25
Jordan <jordan.donguy@gmail.com>
2025-01-15 21:50:18 +0100

    Add components js and module.css files

---
+++ src/Components/App/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./App.module.css";
+import SearchBar from "../SearchBar/SearchBar";
+import SearchResults from "../SearchResults/SearchResults";
+import Playlist from "../Playlist/Playlist";
+
+function App () {
+    return (
+        <div >
+        <h1>
+          Ja<span className={styles.highlight}>mm</span>ing
+        </h1>
+        <div className={styles.App}>
+          {/* <!-- Add a SearchBar component --> */}
+          
+          <div className={styles['.App-playlist']}>
+            {/* <!-- Add a SearchResults component --> */}
+            {/* <!-- Add a Playlist component --> */}
+          </div>
+        </div>
+      </div>
+        );
+}
+
+export default App;
