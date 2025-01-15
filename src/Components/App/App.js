bdc02087cd6cd0e1d205d639a70f9b0905072f2c
Jordan <jordan.donguy@gmail.com>
2025-01-15 21:59:51 +0100

    Create searchResults array with hard-coded initial value

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./App.module.css";
 import SearchBar from "../SearchBar/SearchBar";
 import SearchResults from "../SearchResults/SearchResults";
 import Playlist from "../Playlist/Playlist";
 
 function App () {
+  const [searchResults, setSearchResults] = useState(
+    [
+      {
+        name: "Track 1",
+        artist: "Artist 1",
+        album: "Album 1",
+      },
+      {
+        name: "Track 2",
+        artist: "Artist 2",
+        album: "Album 2",
+      }
+    ]
+  );
     return (
         <div >
         <h1>
@@ -14,7 +28,7 @@ function App () {
           {/* <!-- Add a SearchBar component --> */}
           
           <div className={styles['.App-playlist']}>
-            {/* <!-- Add a SearchResults component --> */}
+            <SearchResults userSearchResults={searchResults} />
             {/* <!-- Add a Playlist component --> */}
           </div>
         </div>
