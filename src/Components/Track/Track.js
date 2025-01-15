cc8d87f3b1246a9a42aefa0502fde722eb93edac
Jordan <jordan.donguy@gmail.com>
2025-01-15 22:16:20 +0100

    Add renderAction method that returns a button

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,6 +2,9 @@ import React from "react";
 import styles from "./Track.module.css";
 
 function Track (props) {
+  function renderAction() {
+    return <button className={styles['.Track-action']}>{props.isRemoval ? "-" : "+"}</button>;
+  };
     return (
       <div className={styles.Track}>
         <div className={styles['.Track-information']}>
