57e32b6853e7b613ff953c8d9364375f97f2b882
Jordan <jordan.donguy@gmail.com>
2025-01-16 17:41:24 +0100

    Create a accesToken variable and a Spotify object that updates the value of accesToken if needed

--- src/util/Spotify/Spotify.js
+++ src/util/Spotify/Spotify.js
@@ -1,2 +1,17 @@
 import Reat from "react";
 
+let accessToken;
+
+const Spotify = {
+    getAccessToken() {
+        if (accessToken) return accessToken;
+        
+        const tokenInURL = window.location.href.match(/access_token=([^&]*)/);
+
+        const expiryTime = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (tokenInURL && expiryTime) {
+            accessToken = tokenInURL[1];
+        }
+    }
+}
