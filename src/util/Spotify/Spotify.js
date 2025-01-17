9e25c1f8f960508aa6299ca031805128f077332d
Jordan <jordan.donguy@gmail.com>
2025-01-17 11:09:11 +0100

    Add a search method to the Spotify object, it's used to request a list of tracks to Spotify as JSON format based on the user search input

--- src/util/Spotify/Spotify.js
+++ src/util/Spotify/Spotify.js
@@ -23,5 +23,15 @@ const Spotify = {
             const redirectURL = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURL}`;
             window.location = redirectURL;
         }
+    },
+
+    search(term) {
+        accessToken = Spotify.getAccessToken();
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+            method: 'GET',
+            header: {authorization: `bearer ${accessToken}`}
+        })
     }
-}
+};
+
+export default Spotify;
