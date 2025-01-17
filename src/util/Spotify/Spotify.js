6535e677465914b039fb667a8b1a735ad6d2b474
Jordan <jordan.donguy@gmail.com>
2025-01-17 11:25:34 +0100

    In the Spotify object, takes the response from spotify and convert it to an array of JSON objects.

--- src/util/Spotify/Spotify.js
+++ src/util/Spotify/Spotify.js
@@ -29,7 +29,20 @@ const Spotify = {
         accessToken = Spotify.getAccessToken();
         return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
             method: 'GET',
-            header: {authorization: `bearer ${accessToken}`}
+            header: { authorization: `bearer ${accessToken}` }
+        })
+        .then(response => response.json())
+        .then(jsonResponse => {
+            if (!jsonResponse) {
+                console.log("Response error")
+            }
+            return jsonResponse.tracks.items.map(t => ({
+                id: t.id,
+                name: t.name,
+                artist: t.artist,
+                album: t.album,
+                URI: t.URI,
+            }))
         })
     }
 };
